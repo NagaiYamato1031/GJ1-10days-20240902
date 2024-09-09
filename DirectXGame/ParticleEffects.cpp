f98bf2eeb7fd602783f06ba979b6a7aa2ab82b77
k022g0055 <k022g0055@g.neec.ac.jp>
2024-09-09 13:39:30 +0900

    見よう見まねでクラス作成_03

--- DirectXGame/ParticleEffects.cpp
+++ DirectXGame/ParticleEffects.cpp
@@ -1,27 +1,41 @@
 #include "ParticleEffects.h"
 
 #include <ImGuiManager.h>
+#include <TextureManager.h>
 
 using namespace ACJPN;
 using namespace ACJPN::Math;
 
+ParticleEffects::~ParticleEffects() { 
+	delete model_; 
+}
+
 void ParticleEffects::Init() {
-	//モデル読み込み
-	model_ = Model::CreateFromOBJ("cube");
-	//初期化
-	transform_.Initialize();
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("white1x1.png");
+
+	//3Dモデルの生成
+	model_ = Model::Create();
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
 }
 
 void ParticleEffects::Update() {
 	ImGui::Begin("ParticleWindow");
 
-	ImGui::DragFloat3("translate", &transform_.translation_.x, 0.1f);
+	ImGui::DragFloat3("translate", &worldTransform_.translation_.x, 0.1f);
 
 	ImGui::End();
 
-	transform_.UpdateMatrix();
+	worldTransform_.UpdateMatrix();
 }
 
 void ParticleEffects::DrawModel(ViewProjection* view) { 	
-	model_->Draw(transform_, *view); 
+	model_->Draw(worldTransform_, *view); 
 }
+
+void ParticleEffects::CreateParticle() {}
+
+void ParticleEffects::UpdateParticle() {}
--- DirectXGame/ParticleEffects.h
+++ DirectXGame/ParticleEffects.h
@@ -9,15 +9,24 @@
 class ParticleEffects {
 public: /*コンストラクタ*/
 	ParticleEffects() = default;
-	~ParticleEffects() = default;
+	~ParticleEffects();
 
 public: /*パブリック関数*/
 
+	//Player.hに書いてあったことをとりあえずそのまま書いた奴{
 	void Init();//初期化
-
 	void Update();//更新
-
 	void DrawModel(ViewProjection* view);//モデル描画
+	// Player.hに書いてあったことをとりあえずそのまま書いた奴}
+
+
+	//頑張って自分で書いた奴{
+
+	void CreateParticle();//パーティクルを生成
+
+	void UpdateParticle();//生成したパーティクルを動かす
+
+	// 頑張って自分で書いた奴}
 
 private: /*メンバ変数*/
 	//入力管理
@@ -27,5 +36,9 @@ private: /*メンバ変数*/
 	Model* model_ = nullptr;
 
 	//ワールド座標
-	WorldTransform transform_
+	WorldTransform worldTransform_;
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+
+	uint32_t textureHandle_ = 0;
 };
