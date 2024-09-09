fb5e3619a99cf5f957bb56645ce97087e631c29f
k022g0055 <k022g0055@g.neec.ac.jp>
2024-09-09 12:59:17 +0900

    見よう見まねでクラス作成_02

--- DirectXGame/ParticleEffects.cpp
+++ DirectXGame/ParticleEffects.cpp
@@ -1,1 +1,27 @@
 #include "ParticleEffects.h"
+
+#include <ImGuiManager.h>
+
+using namespace ACJPN;
+using namespace ACJPN::Math;
+
+void ParticleEffects::Init() {
+	//モデル読み込み
+	model_ = Model::CreateFromOBJ("cube");
+	//初期化
+	transform_.Initialize();
+}
+
+void ParticleEffects::Update() {
+	ImGui::Begin("ParticleWindow");
+
+	ImGui::DragFloat3("translate", &transform_.translation_.x, 0.1f);
+
+	ImGui::End();
+
+	transform_.UpdateMatrix();
+}
+
+void ParticleEffects::DrawModel(ViewProjection* view) { 	
+	model_->Draw(transform_, *view); 
+}
--- DirectXGame/ParticleEffects.h
+++ DirectXGame/ParticleEffects.h
@@ -1,2 +1,31 @@
 #pragma once
-class ParticleEffects {};
+#include <Utility/Mymath.h>
+#include "Input.h"
+#include "Model.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+class ParticleEffects {
+public: /*コンストラクタ*/
+	ParticleEffects() = default;
+	~ParticleEffects() = default;
+
+public: /*パブリック関数*/
+
+	void Init();//初期化
+
+	void Update();//更新
+
+	void DrawModel(ViewProjection* view);//モデル描画
+
+private: /*メンバ変数*/
+	//入力管理
+	Input* input_ = nullptr;
+
+	//3Dモデル
+	Model* model_ = nullptr;
+
+	//ワールド座標
+	WorldTransform transform_
+};
