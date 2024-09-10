ff65e4e937ecc0f9d4e35c76ed631e19c8db8b8c
tsuthiya-tomonori01 <k022g0017@g.neec.ac.jp>
2024-09-10 17:15:30 +0900

    敵から弾が出るのを確認

--- DirectXGame/Boss/Boss.cpp
+++ DirectXGame/Boss/Boss.cpp
@@ -1,4 +1,5 @@
 #include "Boss.h"
+#include <BulletManager/Divide/BulletList.h>
 
 using namespace ACJPN;
 using namespace ACJPN::Math;
@@ -7,13 +8,187 @@ void Boss::Init() {
 	model_.reset(Model::Create());
 	transform_.Initialize();
 	transform_.scale_ = { 10.0f,10.0f,1.0f };
+
+	// 弾管理クラス初期化
+	bulletManager_.Init();
+
+	//ＨＰ・フラグ・フェーズ初期化
+	EnemyHP_ = 40;
+	EnemyIsDead = false;
+	phase_ = p1;
 }
 
 void Boss::Update() {
+
+	switch (phase_) {
+	case Boss::p1:
+		Phase_01();
+		break;
+	case Boss::p2:
+		Phase_02();
+		break;
+	case Boss::p3:
+		Phase_03();
+		break;
+	case Boss::p4:
+		Phase_04();
+		break;
+	}
+
+	// 弾管理クラス更新
+	bulletManager_.Update();
 
 	transform_.UpdateMatrix();
 }
 
 void Boss::DrawModel(ViewProjection* view) {
-	model_->Draw(transform_, *view);
+
+	//弾管理クラス描画
+	bulletManager_.DrawModel(view);
+
+	model_->Draw(transform_, *view); 
 }
+
+//エネミーフェーズ処理
+void Boss::Phase_01() {
+
+	AttackFrame01--;
+
+	//攻撃処理呼び出し
+	if (AttackFrame01 <= 0) {
+
+		EnemyAttack_1();
+		AttackFrame01 = 180;
+	}
+
+	// フェーズ２へ移行
+	if (EnemyHP_ >= 29 && EnemyHP_ <= 30) {
+		phase_ = p2;
+	}
+}
+
+void Boss::Phase_02() {
+
+	AttackFrame01--;
+	AttackFrame02--;
+
+	// 攻撃処理呼び出し
+	if (AttackFrame01 <= 0) {
+
+		EnemyAttack_1();
+		AttackFrame01 = 180;
+	}
+	// 攻撃処理呼び出し
+	else if (AttackFrame02 <= 0) {
+
+		EnemyAttack_2();
+		AttackFrame02 = 60;
+	}
+
+	//フェーズ３へ移行
+	if (EnemyHP_ >= 19 && EnemyHP_ <= 28) {
+		phase_ = p3;
+	}
+}
+
+void Boss::Phase_03() {
+
+	// 攻撃処理呼び出し
+	if (false) {
+
+		EnemyAttack_1();
+	}
+	// 攻撃処理呼び出し
+	else if (false) {
+
+		EnemyAttack_3();
+	}
+
+	// フェーズ４へ移行
+	if (EnemyHP_ >= 9 && EnemyHP_ <= 18) {
+		phase_ = p4;
+	}
+}
+
+void Boss::Phase_04() {
+
+	// 攻撃処理呼び出し
+	if (false) {
+
+		EnemyAttack_1();
+	}
+	// 攻撃処理呼び出し
+	else if (false) {
+
+		EnemyAttack_2();
+	} 
+	// 攻撃処理呼び出し
+	else if (false) {
+
+		EnemyAttack_3();
+	}
+
+	//エネミーのHPが０以下になったらEndSceneに移行
+	if (EnemyHP_ >= 0 && EnemyHP_ <= 9) {
+		
+		//フラグをTrueにする
+		EnemyIsDead = true;
+	}
+}
+
+// エネミー攻撃処理
+void Boss::EnemyAttack_1() {
+
+	// プレイヤーを狙って飛んでくる弾
+	SimpleBullet* data = new SimpleBullet;
+	data->Init();
+	data->transform_.translation_ = transform_.translation_;
+	// 弾とプレイヤーの距離を計算する
+	data->velocity_.x = std::cosf(theta_) * -1.5f;
+	data->velocity_.y = std::sinf(theta_) * -1.5f;
+	// 登録
+	bulletManager_.Regist(data);
+}
+
+void Boss::EnemyAttack_2() {
+
+	// プレイヤーの周りを狙って飛んでくる弾
+	SimpleBullet* data = new SimpleBullet;
+	data->Init();
+	data->transform_.translation_ = transform_.translation_;
+	// 弾をプレイヤーの方向に向ける
+	data->velocity_.x;
+	data->velocity_.y;
+	// 登録
+	bulletManager_.Regist(data);
+}
+
+void Boss::EnemyAttack_3() {
+
+	// 円に向かって飛び、波を発生させる弾
+	SimpleBullet* data = new SimpleBullet;
+	data->Init();
+	data->transform_.translation_ = transform_.translation_;
+	// 弾をプレイヤーの方向に向ける
+	data->velocity_.x;
+	data->velocity_.y;
+	// 登録
+	bulletManager_.Regist(data);
+}
+
+
+void Boss::SceneLoad() {
+
+	//EnemyIsDeadがtrueの時に
+	if (EnemyIsDead == true) {
+
+		/*nextScene_ = ;
+		sceneFlag_.isTransition_ = true;
+		sceneFlag_.allEnd_ = true;
+
+		EnemyIsDead = false;*/
+	}
+}
+
+
+
--- DirectXGame/Boss/Boss.h
+++ DirectXGame/Boss/Boss.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <memory>
+
+#include "SceneManager/IScene.h"
+
 #include <Utility/Mymath.h>
 #include "Audio.h"
 #include "Input.h"
@@ -7,12 +11,13 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include <BulletManager/BulletManager.h>
 
 /// <summary>
 /// ボス
 /// 後で仮想化するかもしれない
 /// </summary>
-class Boss {
+class Boss : public ACJPN::Scene::IScene {
 public: //** コンストラクタ **//
 
 	/// <summary>
@@ -49,7 +54,54 @@ private: //** プライベート変数 **//
 	// ワールド座標
 	WorldTransform transform_;
 
-	// 当たり判定
+	// 弾管理
+	BulletManager bulletManager_;
 
+	//エネミー体力
+	int EnemyHP_ = 40;
+
+	//エネミー攻撃遷移フレーム
+	int AttackFrame01 = 180;
+	int AttackFrame02 = 60;
+	int AttackFrame03 = 180;
+
+
+	// 中心からの回転角
+	float theta_ = 0.0f;
+
+	//エネミー死亡判定
+	bool EnemyIsDead = false;
+
+	//フェーズ一覧
+	enum Phase {
+		p1,
+		p2,
+		p3,
+		p4,
+	};
+
+	//現在の行動
+	Phase phase_ = Phase::p1;
+
+private:
+
+	//エネミー攻撃手段
+	void EnemyAttack_1(); //自機狙い
+
+	void EnemyAttack_2(); //自機周辺狙い
+
+	void EnemyAttack_3(); //波発生
+
+	//エネミーフェーズ
+	void Phase_01(); //第一段階
+
+	void Phase_02(); //第二段階
+
+	void Phase_03(); //第三段階
+
+	void Phase_04(); //最終段階
+
+	//シーン遷移
+	void SceneLoad(); //EndSceneに移行する関数
 
 };
