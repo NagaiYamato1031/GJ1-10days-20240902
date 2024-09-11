b9a507b9205501d0c3c6182218e4ad30cb4d4917
NagaiYamato <115054936+NagaiYamato1031@users.noreply.github.com>
2024-09-11 15:14:55 +0900

    ボスに当たり判定

--- DirectXGame/Collider/CollisionManager.cpp
+++ DirectXGame/Collider/CollisionManager.cpp
@@ -1,5 +1,7 @@
 #include "CollisionManager.h"
 
+#include "MaskCollision.h"
+
 using namespace ACJPN;
 using namespace ACJPN::Collider;
 
@@ -17,22 +19,38 @@ void CollisionManager::Update() {
 	// すべて走査する
 	for (auto itr = colliderMap_.begin(); itr != colliderMap_.end(); ++itr) {
 		// マスクごとに処理
-		for (std::weak_ptr<ShapeColliderBase>& collider : itr->second) {
-			// 一時的に参照する
-			if (std::shared_ptr<ShapeColliderBase> r = collider.lock()) {
-				r->Update();
+		itr->second.remove_if([](std::weak_ptr<ShapeColliderBase>& ptr) {
+			// 参照が消えているなら
+			if (ptr.expired()) {
+				return true;
 			}
-		}
+			return false;
+			});
 	}
 }
 
-template<typename T>
-inline void CollisionManager::RegistCollider(int mask, std::shared_ptr<ShapeCollider<T>> collider) {
-	colliderMap_[mask].emplace_back(collider);
+void CollisionManager::CheckCollision() {
+
+	//** 完全に手動でマスク処理を分ける **//
+
+	// プレイヤーと敵の弾
+
+	// ボスとプレイヤーの弾
+	CheckCollisionBoss();
 }
 
-void CollisionManager::CheckCollision() {
-	// 完全に手動でマスク処理を分ける
-
+void ACJPN::Collider::CollisionManager::CheckCollisionBoss() {
+	// ボスの当たり判定
+	std::weak_ptr<ShapeColliderBase>& boss = colliderMap_[MBoss()].front();
+	// 中身が無かったら処理しない
+	if (boss.expired()) {
+		return;
+	}
+	// プレイヤーの弾の当たり判定
+	if (std::shared_ptr<ShapeColliderBase> ptr = boss.lock()) {
+		for (auto listItr = colliderMap_[MPlayerBullet()].begin(); listItr != colliderMap_[MPlayerBullet()].end(); ++listItr) {
+			ptr->CheckCollision(listItr->lock().get());
+		}
+	}
 
 }
