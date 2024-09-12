7a25cd79d2fb1b930fbd17e19665b938abeafe53
tsuthiya-tomonori01 <k022g0017@g.neec.ac.jp>
2024-09-12 14:56:45 +0900

    スコアの処理を追加

--- DirectXGame/Score.cpp
+++ DirectXGame/Score.cpp
@@ -1,7 +1,16 @@
 #include "Score.h"
+#include <stdio.h>
 
-void Score::Init() {}
+void Score::Init() {
+
+	//０で初期化
+	GameScore_ = 0; 
+}
 
 void Score::Update() {}
 
-void Score::Draw() {}
+void Score::Draw() {
+
+	char str[100];
+	sprintf_s(str, "SCORE %d", GameScore_);
+}
--- DirectXGame/Score.h
+++ DirectXGame/Score.h
@@ -20,6 +20,14 @@ public:
 	void Draw();
 
 private:
+	
+	//スコア変数
+	int GameScore_ = 0;
 
+private:
+
+	//Getter・Setter
+	int GetGameScore() { return GameScore_; }
+	void SetGameScore(int Score) { GameScore_ = Score; };
 
 };
