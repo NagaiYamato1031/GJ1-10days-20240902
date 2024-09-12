eaef6dd5ec3c4b667d53cfe935343a1c34522ad9
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2024-09-12 18:50:44 +0900

    途中

--- DirectXGame/Boss/CreateHpbar.cpp
+++ DirectXGame/Boss/CreateHpbar.cpp
@@ -1,1 +1,24 @@
 #include "CreateHpbar.h"
+
+#include <ImGuiManager.h>
+#include <TextureManager.h>
+
+using namespace ACJPN;
+using namespace ACJPN::Math;
+
+void CreateHpbar::Init(const Vector3& pos, Model *model ) {
+	//破壊フラグ初期化
+	isBreak = false;
+	model_ = model;
+	textureHandle_ = TextureManager::Load("HpBar/HpBar");
+	
+
+	worldTransform_.Initialize();
+	worldTransform_.translation_ = pos;
+}
+
+void CreateHpbar::Update() {  }
+
+void CreateHpbar::Draw(const ViewProjection* viewProjection) { 
+	model_->Draw(worldTransform_, *viewProjection, textureHandle_);
+}
--- DirectXGame/Boss/CreateHpbar.h
+++ DirectXGame/Boss/CreateHpbar.h
@@ -1,2 +1,24 @@
 #pragma once
-class CreateHpbar {};
+#include <Utility/Mymath.h>
+#include <Model.h>
+#include <WorldTransform.h>
+class CreateHpbar {
+public: /*コンストラクト*/
+	CreateHpbar()=default;
+	~CreateHpbar()=default;
+
+public: /*パブリック関数*/
+	void Init(const Vector3& pos, Model* model_);
+	void Update();
+	void Draw(const ViewProjection *viewProjection);
+
+private: /*メンバ変数*/
+	//3Dモデル
+	Model* model_;
+	//ワールド座標
+	WorldTransform worldTransform_;
+	//破壊フラグ
+	bool isBreak;
+	//テクスチャハンドル
+	TextureManager textureHandle_;
+};
